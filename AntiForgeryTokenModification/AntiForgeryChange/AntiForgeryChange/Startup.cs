b584f206c490b29e003d074fadb746ed45295ca3
suman89 <sumannandi.1989@gmail.com>
2018-01-05 09:32:15 +0530

    Antiforgerytoken update solution created

---
+++ AntiForgeryTokenModification/AntiForgeryChange/AntiForgeryChange/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AntiForgeryChange.Startup))]
+namespace AntiForgeryChange
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
