0948836f2c7ca087cd4a317fee681d482505e6a4
suman89 <sumannandi.1989@gmail.com>
2018-01-05 10:07:13 +0530

    changes in anti forgery token modifictaion

---
+++ AntiForgeryTokenModification/AntiForgeryChange/AntiForgeryChange/Common/FormExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace AntiForgeryChange.Common
+{
+    public static class FormExtensions
+    {
+
+        public static MvcForm BeginDataForm(this HtmlHelper html, string action, string controller, FormMethod method, object htmlAttributes)
+        {
+            var form = html.BeginForm(action, controller, method, htmlAttributes);
+            //At this point, the form markup is rendered in BeginForm
+            // we can render the token
+
+            //With every form, we render a token, since this
+            //assumes all forms are posts
+            html.ViewContext.Writer.Write(html.AntiForgeryToken().ToHtmlString());
+
+            return form;
+        }
+
+        public static MvcHtmlString ModifiedAntiForgeryToken(this HtmlHelper html)
+        {
+            var test = html.AntiForgeryToken();
+            var check = test.ToHtmlString();
+            var check1 = test.ToString();
+            return test;
+        }
+    }
+}
